0f40dfe4f7dc43c0c3c19dad0f80608c2ccc1923
kirscheeh <kirsten-goebel@gmx.de>
2025-12-08 22:52:23 +0100

    day 8

--- 2025/python/day08.py
+++ 2025/python/day08.py
@@ -22,15 +22,26 @@ def get_distances(boxes:list) -> list[tuple[int, int, float]]:
     
     return sorted(dist, key=lambda x: x[2])
 
-def connect_x_boxes(eucl_dist:list[tuple], number) -> dict:
+def connect_x_boxes(eucl_dist:list[tuple], boxes, number, p2=False) -> dict:
     # returns circuits
     circuits = {}
-    number_circuits = 0
-    for (box1, box2, _) in eucl_dist:
+    number_circuits = 1
+    not_in_circuit = {box:True for box in boxes}
+    first = True
+    if p2:
+        loop_range = len(eucl_dist)+20
+    else:
+        loop_range=number
+
+    for (box1, box2, _) in eucl_dist[:loop_range]:
+
         if (nr_circuit:=circuits.get(box1)) and not circuits.get(box2): # one is in circuit
             circuits[box2] = nr_circuit
+            not_in_circuit[box2]=False
+
         elif (nr_circuit:=circuits.get(box2)) and not circuits.get(box1): # two is in circuit
             circuits[box1] = nr_circuit
+            not_in_circuit[box1]=False
         elif (nr_circuit1:=circuits.get(box1)) and (nr_circuit2:=circuits.get(box2)): # both are in circuit
             if nr_circuit1 == nr_circuit2: # same circuit
                 pass
@@ -41,10 +52,13 @@ def connect_x_boxes(eucl_dist:list[tuple], number) -> dict:
         else: # non on circuit
             circuits[box1] = number_circuits
             circuits[box2] = number_circuits
+            not_in_circuit[box1] = False
+            not_in_circuit[box2] = False
             number_circuits +=1
-            
-    if len(set(circuits.values())) == 1:
-        print(box1, box2)
+
+        if sum(list(not_in_circuit.values())) == 0 and p2 and first:
+            print("Part2", box1[0]*box2[0])
+            return 0
         
     return circuits
 
@@ -56,8 +70,9 @@ def get_circuit_sizes(circuits: dict) -> list[int]:
 def get_bridge_boxes(circuits: dict):
     pass
 
+boxes = read_boxes(junction_boxes)
+sorted_euclidian_distances = get_distances(boxes)
 
-sorted_euclidian_distances = get_distances(read_boxes(junction_boxes))
-circuits = connect_x_boxes(sorted_euclidian_distances, 10)
-
-print("Part 1", math.prod(get_circuit_sizes(circuits)[-3:]))
+circuits = connect_x_boxes(sorted_euclidian_distances, boxes, 1000, True)
+if not circuits == 0:
+    print("Part 1", math.prod(get_circuit_sizes(circuits)[-3:]))
