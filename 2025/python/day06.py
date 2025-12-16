390dc3e657dc0662cc3ff350e2f87106f2d032f3
kirscheeh <kirsten-goebel@gmx.de>
2025-12-16 13:43:19 +0100

    parsing horror for day6 cephalopod maths

--- 2025/python/day06.py
+++ 2025/python/day06.py
@@ -29,6 +29,34 @@ for index, sign in enumerate(signs):
 
 
 print("Part 1", total)
+
 ## part2
 
+data = [m[::-1] for m in data]
+
+numbers = []
+results = []
+
 # well i hate cephalopod math
+for test in zip(*data):
+    print(test)
+    
+    if (num:="".join(test).strip()) == "":
+        continue
+    
+    sign = test[-1]
+    if sign in num:
+        num = num[:-1]
+    print(num)
+    numbers.append(int(num))
+    match sign:
+        case "+": 
+            results.append(sum(numbers))
+            numbers = []
+        case "*": 
+            results.append(math.prod(numbers))
+            numbers = []
+        case _:
+            continue
+
+print("Part 2", sum(results))
